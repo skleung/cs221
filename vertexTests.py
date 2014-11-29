8e00dc6cddebc9476f7425dbaca213f3fc72d047
Nick Troccoli <nicktroccoli28@gmail.com>
2014-11-29 15:42:05 -0500

    Added test files for Edge, Hexagon, and Vertex to test functionality and exception raising for each.  Just run test.py to run all tests across the 3 classes.

---
+++ vertexTests.py
@@ -0,0 +1,214 @@
+from board import *
+
+
+class VertexTests:
+	"""
+	Class: VertexTests
+	--------------------------
+	A class containing all tests for the Vertex class.
+	--------------------------
+	"""
+
+	def __init__(self):
+		pass
+
+	def testVertexInit(self):
+		"""
+		Method: testVertexInit
+		--------------------------
+		Tests to make sure all instance variables are properly
+		initialized.
+		--------------------------
+		"""
+		print "Running testVertexInit....."
+
+		v1 = Vertex(1, 2)
+		assert(v1.X is 1)
+		assert(v1.Y is 2)
+		assert(v1.player is None)
+		assert(not v1.isSettlement)
+		assert(not v1.isCity)
+
+		v1 = Vertex(3, 4)
+		assert(v1.X is 3)
+		assert(v1.Y is 4)
+		assert(v1.player is None)
+		assert(not v1.isSettlement)
+		assert(not v1.isCity)
+
+	def testVertexIsOccupied(self):
+		"""
+		Method: testVertexIsOccupied
+		--------------------------
+		Tests to make sure a Vertex correctly reports whether or
+		not it is occupied.
+		--------------------------
+		"""
+		print "Running testVertexIsOccupied...."
+
+		# Not occupied initially
+		v1 = Vertex(3, 4)
+		assert(not v1.isOccupied())
+
+		# Settle (now occupied)
+		v1.settle(2)
+		assert(v1.isOccupied())
+
+		# Upgrade (still occupied)
+		v1.upgrade(2);
+		assert(v1.isOccupied())
+
+	def testVertexCopy(self):
+		"""
+		Method: testVertexCopy
+		--------------------------
+		Tests to make sure the deepCopy method on Vertex
+		properly deep copies a given vertex.
+		--------------------------
+		"""
+		print "Running testVertexCopy....."
+
+		v1 = Vertex(1, 2)
+		v1.player = 5
+		v2 = v1.deepCopy()
+
+		# Make sure they're not the same object
+		assert(v1 != v2)
+
+		# Make sure X is not changed
+		v1.X = 0
+		assert(v2.X != 0)
+
+		# Make sure Y is not changed
+		v1.Y = 3
+		assert(v2.Y != 3)
+
+		# Make sure player is not changed
+		v1.player = 6
+		assert(v2.player != 6)
+
+		# Make sure settlement/city booleans are not changed
+		v1.isSettlement = True
+		assert(not v2.isSettlement)
+
+		v1.isCity = True
+		assert(not v2.isCity)
+
+	def testVertexSettle(self):
+		"""
+		Method: testVertexSettle
+		--------------------------
+		Tests to make sure a Vertex's instance variables
+		are properly updated when settling, and that
+		improperly settling correctly raises an exception.
+		--------------------------
+		"""
+		print "Running testVertexSettle...."
+
+		v1 = Vertex(1, 2)
+
+		# Make sure settling properly updates state
+		v1.settle(2)
+		assert(v1.player is 2)
+		assert(v1.isSettlement)
+		assert(not v1.isCity)
+
+		# Make sure you can't settle twice (this should
+		# raise an exception but be caught)
+		try:
+			v1.settle(2)
+			print "Error: allowed multiple settles"
+		except:
+			# Make sure nothing about the Vertex changed
+			assert(v1.player is 2)
+			assert(v1.isSettlement)
+			assert(not v1.isCity)
+
+	def testVertexUpgrade(self):
+		"""
+		Method: testVertexUpgrade
+		--------------------------
+		Tests to make sure a Vertex's instance variables
+		are properly updated when upgrading from a settlement
+		to a city, and that improperly settling correctly
+		raises an exception.
+		--------------------------
+		"""
+		print "Running testVertexUpgrade....."
+
+		# ---- Minitest 1: Check valid upgrading ----
+		v1 = Vertex(3, 4)
+
+		# Make sure upgrading properly updates state
+		v1.settle(3)
+		v1.upgrade(3)
+		assert(v1.player is 3)
+		assert(not v1.isSettlement)
+		assert(v1.isCity)
+		# ---- End Minitest 1 ---- #
+
+
+		# ---- Minitest 2: Make sure you can't upgrade twice (this should
+		# raise an exception but be caught) ----
+		try:
+			v1.upgrade(3)
+			print "Error: allowed multiple upgrades"
+		except:
+			# Make sure nothing about the Vertex changed
+			assert(v1.player is 3)
+			assert(not v1.isSettlement)
+			assert(v1.isCity)
+		# ---- End Minitest 2 ---- #
+
+
+		# ---- Minitest 3: Make sure two different players can't build
+		# a settlement here and upgrade it ----
+		v1 = Vertex(3, 4)
+		v1.settle(3)
+
+		# Make sure two different players can't build (this
+		# should raise an exception but be caught)
+		try:
+			v1.upgrade(4)
+			print "Error: two different players allowed to settle/upgrade"
+		except:
+			# Make sure nothing about the Vertex changed
+			assert(v1.player is 3)
+			assert(v1.isSettlement)
+			assert(not v1.isCity)
+		# ---- End Minitest 3 ---- #
+
+
+		# ---- Minitest 4: Make sure the Vertex has a settlement before
+		# upgrading ----
+		v1 = Vertex(3, 4)
+
+		# Make sure a settlement is present before upgrading (this
+		# should raise an exception but be caught)
+		try:
+			v1.upgrade(4)
+			print "Error: upgraded without settlement!"
+		except:
+			# Make sure nothing about the Vertex changed
+			assert(v1.player is None)
+			assert(not v1.isSettlement)
+			assert(not v1.isCity)
+		# ---- End Minitest 4 ---- #
+
+	def runAllTests(self):
+		"""
+		Method: runTests
+		--------------------------
+		Run all tests for this test class.
+		--------------------------
+		"""
+		print "Running Vertex tests....."
+		print "-------------------------"
+		
+		self.testVertexCopy()
+		print "Success!"
+		self.testVertexSettle()
+		print "Success!"
+		self.testVertexUpgrade()
+		print "Success!"
+
