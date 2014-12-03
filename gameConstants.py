190e8a9213c860c31861669ba301632f3d313f68
Nick Troccoli <nicktroccoli28@gmail.com>
2014-12-03 01:31:01 -0800

    Revert "Reverted pqueue"
    This reverts commit b0a2be8fbc7b1908c0b90c31bf394b9ff9a904aa.

--- gameConstants.py
+++ gameConstants.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from collections import Counter
 
-GRAPHICS = True
+GRAPHICS = False
 # Moved here because not in draw
 def getColorForPlayer(player):
     return {
@@ -18,6 +18,7 @@ CITY_VICTORY_POINTS = SETTLEMENT_VICTORY_POINTS + 1
 NUM_INITIAL_SETTLEMENTS = 2
 TOTAL_NUM_AGENTS = 13
 CUTOFF_TURNS = 600
+CUTOFF_ACTIONS = 2
 
 VERBOSE = False
 
