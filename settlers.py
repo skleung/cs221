ab976561f0515c97b711e61dfa1bca6942cc8f59
Sherman Leung <shermankleung@gmail.com>
2014-12-09 16:59:26 -0800

    added statistics tracking

--- settlers.py
+++ settlers.py
@@ -10,7 +10,10 @@ def play(n=100, human=False, randomFlag=True):
     player = game.players[0]
     computer = game.players[1]
     graphics = False
+
+    numTurns = 0
     while not game.terminal(state):
+        numTurns += 1
         if graphics: game.pretty_state(state)
         if human:
             prompt = 'Choose a move, choices are %s: ' % (game.actions(state, player))
@@ -25,15 +28,18 @@ def play(n=100, human=False, randomFlag=True):
                     pass
                 except Exception:
                     pass
+            print 'Player 1 (Human) chose ' + str(action)
         elif randomFlag:
             actions = game.actions(state, player)
             action = random.choice(actions)
             state = game.result(state, action, player)
+            print 'Player 1 (Random) chose ' + str(action)
         # defaults to MCTS agent...
         else:
             action = mcts.mcts_uct(game, state, player, n)
             state = game.result(state, action, player)
-        print 'Player 1 chose ' + str(action)
+            print 'Player 1 (MCTS) chose ' + str(action)
+        
         if graphics: game.pretty_state(state)
 
         # Intermediate win check
@@ -44,18 +50,21 @@ def play(n=100, human=False, randomFlag=True):
         ts = time.time()
         action = mcts.mcts_uct(game, state, computer, n)
         state = game.result(state, action, computer)
-
         print 'Player 2 (MCTS) chose ' + str(action) + " (elapsed time="+str(time.time()-ts)+")"
 
     if graphics: game.pretty_state(state)
     print
     outcome = game.outcome(state, player)
+    ptDeficit = abs(game.game.gameState.playerAgents[0].victoryPoints - game.game.gameState.playerAgents[1].victoryPoints)
     if outcome == 1:
         print 'Player 1 wins.'
+        return (1, numTurns, ptDeficit)
     elif outcome == -1:
-        print 'Player 2 wins.'
+        print 'Player 2 (MCTS) wins.'
+        return (2, numTurns, ptDeficit)
     else:
         print 'Tie game.'
+    return (0, numTurns, ptDeficit)
     
 
 n = 100
@@ -82,8 +91,63 @@ if '-r' in sys.argv:
 
 
 START_TIME = time.time()
-print 'Number of Sample Iterations: ' + str(n)
+print 'Number of Total Iterations: ' + str(n)
+print 'Number of Sample simulations for MCTS algorithm: ' + str(50)
 print 'Human Player: ' + str(human)
 print 'Random Player: ' + str(randomFlag)
-play(n=n, human=human, randomFlag=randomFlag)
-print "time elapsed = "+str(time.time()-START_TIME)
+
+
+
+mctsWins = 0
+mctsTurns = []
+mctsTimes = []
+mctsPtDeficit = []
+
+randomWins = 0
+randomTurns = []
+randomTimes = []
+randomPtDeficit= []
+
+weirdGames = 0
+for i in range(n):
+    whoWon = 0
+    nTurns = 0
+    ptDeficit = 0
+    startTime = time.time()
+    try:
+        # Note: I'm hardcoding our number of sample iterations to 50 here!
+        whoWon, nTurns, ptDeficit = play(n=50, human=human, randomFlag=randomFlag)
+    except Exception:
+        whoWon = 0
+        print "EXCEPTION WAS THROWN"
+        pass
+    timeElapsed = time.time()-startTime
+    print "Game #"+str(i)+": time elapsed = "+str(timeElapsed)
+    if (whoWon == 2):
+        mctsWins += 1
+        mctsTurns.append(nTurns)
+        mctsTimes.append(timeElapsed)
+        mctsPtDeficit.append(ptDeficit)
+    elif (whoWon == 1):
+        randomWins += 1
+        randomTurns.append(nTurns)
+        randomTimes.append(timeElapsed)
+        randomPtDeficit.append(ptDeficit)
+    else:
+        weirdGames += 1
+print "===================="
+print "Overall statistics: "
+print str(mctsWins) + " wins for MCTS"
+print "Average time to win for MCTS =" + str(sum(mctsTimes)/len(mctsTimes))
+print "Average turns to win for MCTS =" + str(sum(mctsTurns)/len(mctsTurns))
+print "Average ptDecifit to win for MCTS =" + str(sum(mctsPtDeficit)/len(mctsPtDeficit))
+
+print str(randomWins) + " wins for Random player"
+print "Average time to win for Random Player =" + str(sum(randomTimes)/len(randomTimes))
+print "Average turns to win for Random Player =" + str(sum(randomTurns)/len(randomTurns))
+print "Average ptDecifit to win for Random Player =" + str(sum(randomPtDeficit)/len(randomPtDeficit))
+print
+
+
+print str(weirdGames) + " weird games happened"
+print str(mctsWins/float(mctsWins+randomWins)) +" %% win rate by MCTS agent"
